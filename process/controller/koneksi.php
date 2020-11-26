9781b4688bc9cc913c250a9f71b2ff69955b95a8
Fachri <fachri@adakerja.com>
2020-11-26 16:31:10 +0700

    update conn

--- process/controller/koneksi.php
+++ process/controller/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$username="root";
-$password=""; //kosongkan jika tidak ada
-$host="localhost";
-$database="kelurahan";
+$username="iaihj08v0xo3puw5";
+$password="ifr2x52nfmoof5fb"; //kosongkan jika tidak ada
+$host="aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$database="qy8m3hqb9a21fcph";
 $conn= mysql_connect($host,$username,$password) or die ("koneksi server gagal");
 mysql_select_db($database) or die ("koneksi database gagal");
 ?>
